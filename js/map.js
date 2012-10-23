2afa62aa2d64c3ff9b7746a5ad9c1427bd253d71
Matthew Holloway <matth@catalyst.net.nz>
2012-10-23 13:24:39 +1300

    Automated commit

--- js/map.js
+++ js/map.js
@@ -9,18 +9,30 @@
                             'Heading: '            + position.coords.heading               + ',' +
                             'Speed: '              + position.coords.speed                 + ',' +
                             'Timestamp: '          + new Date(position.timestamp)          + '.');
+
+		if(window.map_details === undefined) {
+			return;
+		}
+		var youarehere_css = {
+			"position":"absolute",
+			"left": -parseInt((position.coords.latitude - window.map_details.latitude) / window.map_details.degrees_per_pixel, 10) +  "px",
+			"top": parseInt((position.coords.longitude - window.map_details.longitude) / window.map_details.degrees_per_pixel, 10)  + "px",
+		};
+		$("#youarehere").css(youarehere_css);
+		$(".navbar-inner").append(JSON.stringify(youarehere_css));
+
 	}
 
 	var geolocationError = function(msg) {
-		alert(JSON.stringify(msg));
+		$(".navbar-inner").html(JSON.stringify(msg));
 	}
 
 	var watchId = navigator.geolocation.watchPosition(geolocationSuccess, geolocationError, {
 		maximumAge:600000,
-		timeout:5000,
+		
 		enableHighAccuracy: true});
 
 	$(".navbar-inner").html("Checking GPSy");
 
-	
+
 }())
