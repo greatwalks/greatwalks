7b29752acd3c2919d1cd331486bba0c981dc4787
Matthew Holloway <matth@catalyst.net.nz>
2012-10-23 13:56:36 +1300

    Automated commit

--- js/map.js
+++ js/map.js
@@ -1,31 +1,47 @@
 (function(){
 	"use strict";
-	var geolocationSuccess = function(position){
-		$(".navbar-inner").html( 'Latitude: '           + position.coords.latitude              + ',' +
-                            'Longitude: '          + position.coords.longitude             + ',' +
-                            'Altitude: '           + position.coords.altitude              + ',' +
-                            'Accuracy: '           + position.coords.accuracy              + ',' +
-                            'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + ',' +
-                            'Heading: '            + position.coords.heading               + ',' +
-                            'Speed: '              + position.coords.speed                 + ',' +
-                            'Timestamp: '          + new Date(position.timestamp)          + '.');
-
-		if(window.map_details === undefined) {
-			return;
-		}
-		var youarehere_css = {
-			"position":"absolute",
-			"left": -parseInt((position.coords.latitude - window.map_details.latitude) / window.map_details.degrees_per_pixel, 10) +  "px",
-			"top": parseInt((position.coords.longitude - window.map_details.longitude) / window.map_details.degrees_per_pixel, 10)  + "px",
-		};
-		$("#youarehere").css(youarehere_css);
-		$(".navbar-inner").append(JSON.stringify(youarehere_css));
-
+	if($(".map").length === 0) {
+		return;
 	}
+	var centered_on_load = false,
+		geolocationSuccess = function(position){
+			$(".navbar-inner").html( 'Latitude: '           + position.coords.latitude              + ',' +
+	                            'Longitude: '          + position.coords.longitude             + ',' +
+	                            'Altitude: '           + position.coords.altitude              + ',' +
+	                            'Accuracy: '           + position.coords.accuracy              + ',' +
+	                            'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + ',' +
+	                            'Heading: '            + position.coords.heading               + ',' +
+	                            'Speed: '              + position.coords.speed                 + ',' +
+	                            'Timestamp: '          + new Date(position.timestamp)          + '.');
 
-	var geolocationError = function(msg) {
+			if(window.map_details === undefined) {
+				return;
+			}
+			var pixels = {
+				"left": -parseInt((position.coords.latitude - window.map_details.latitude) / window.map_details.degrees_per_pixel, 10),
+				"top": parseInt((position.coords.longitude - window.map_details.longitude) / window.map_details.degrees_per_pixel, 10)
+			} 
+			var youarehere_css = {
+				"position":"absolute",
+				"left": pixels.left + "px",
+				"top": pixels.top + "px",
+			};
+			$("#youarehere").css(youarehere_css);
+			$(".navbar-inner").append(JSON.stringify(youarehere_css));
+
+			if(centered_on_load === false) {
+				window.scrollTo(
+					pixels.left - $(window).width() / 2,
+					pixels.top - $(window).height() / 2
+				);
+				centered_on_load = true;
+			}
+		},
+		geolocationError = function(msg) {
 		$(".navbar-inner").html(JSON.stringify(msg));
 	}
+
+	
 
 	var watchId = navigator.geolocation.watchPosition(geolocationSuccess, geolocationError, {
 		maximumAge:600000,
@@ -37,5 +53,7 @@
 	$(".location span").click(function(){
 		$(this).next("h3").toggle();
 	});	
+
+
 
 }())
