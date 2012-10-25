fc31d9dec789e5bbec362a809cc9e0b9dcb31dc5
Matthew Holloway <matth@catalyst.net.nz>
2012-10-25 16:21:19 +1300

    Automated commit

--- js/map.js
+++ js/map.js
@@ -156,8 +156,8 @@
 		        });
 
 		   		hammer.bind('drag', function(event) {
-		   			offset.left += event.distanceX;
-		   			offset.top += event.distanceY;
+		   			translate.x += event.distanceX;
+		   			translate.y += event.distanceY;
 		   			$image.css('-webkit-transform', 'translate3d(' + event.distanceX + 'px, ' + event.distanceY + 'px, 0) scale3d(' + scale + ', ' + scale + ', 1)');
 		   		});
 
