d0686c9abc8ba02169ab6d1410126133ef6337b5
Matthew Holloway <matth@catalyst.net.nz>
2012-10-25 16:17:34 +1300

    Automated commit

--- js/map.js
+++ js/map.js
@@ -179,7 +179,7 @@
 		            translate.x += -origin.x * (newWidth - width) / newWidth;
 		            translate.y += -origin.y * (newHeight - height) / newHeight;
 
-		            $image.css('-webkit-transform', "scale3d(" + scale + ", " + scale + ", 1)"); //translate3d(" + translate.x + "px, " + translate.y + "px, 0)");
+		            $image.css('-webkit-transform', "scale3d(" + scale + ", " + scale + ", 1) translate3d(" + offset.left + "px, " + offset.top + "px, 0)");
 		            width = newWidth;
 
 		            return height = newHeight;
